1541051e098c433bbea5a57493658a2f9cafe224
jeremiahalajemba <JeremiahAlajemba@gmail.com>
2024-05-01 13:45:23 +0100

    Created the constructor for the Patient class and initialised the fields for the Patient class in the constructor and created the getters and setters for the fields

--- src/main/java/Patient.java
+++ src/main/java/Patient.java
@@ -12,5 +12,53 @@ public class Patient {
 
     }
 
+    public Patient(String firstName, String lastName, LocalDate dateOfBirth, LocalDate joinedDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dateOfBirth = dateOfBirth;
+        this.joinedThePractice = joinedThePractice;
+        this.appointments = new LinkedList<>();
+    }
+
+    // Getters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        if (firstName != null) {
+            this.firstName = firstName;
+        }
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        if (lastName != null){
+            this.lastName = lastName;
+        }
+    }
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        if (dateOfBirth.isEqual(LocalDate.now()) || dateOfBirth.isBefore(LocalDate.now())){
+            this.dateOfBirth = dateOfBirth;
+        }
+    }
+    public LocalDate getJoinedThePractice() {
+        return joinedThePractice;
+    }
+    public void setJoinedThePractice(LocalDate joinedThePractice) {
+        this.joinedThePractice = joinedThePractice;
+    }
+    public LinkedList<Appointment> getAppointments() {
+        return appointments;
+    }
+    public void setAppointments(LinkedList<Appointment> appointments) {
+        this.appointments = appointments;
+    }
+
+
+
 
 }
